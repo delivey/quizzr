3392ff5e570eae61c425eccd863be60dea8b719a
delivey <deliveydev@gmail.com>
2020-05-20 19:24:41 +0300

    optimized question addage

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-open_file = open('D:/Quizzes/sample_quiz.txt', 'r') # change to your directory
+open_file = open('C:/Users/Domantas/Desktop/quizzr/sample_quiz.txt', 'r') # change to your directory
 read_file = open_file.read()
 quiz_file = read_file.splitlines()
 
 numbers = ["1. ", "2. ", "3. ", "4. ", "5. ", "6. ", "7. ", "8. ", "9. ", "10. ", "11. ", "12. ", "13. ", "14. ", "15. "]
+right_answers = 0
 
 class Question:
     def __init__(self, qumber, asnum):
         self.qumber = qumber
         self.asnum = asnum
     def quiz(self, qumber, asnum):
+        global right_answers
         questions = []
         real_answers = []
         for line in quiz_file:
@@ -30,34 +32,20 @@ class Question:
         right_answer = fixed_answer[:digit]
         if answer == right_answer:
             print("Your answer is correct!")
+            right_answers += 1
         else:
             print("Your answer is incorrect.")
 
-ob = Question(qumber = "1.", asnum = 0) # asnum starts from 0
-ob.qumber = "1."  # question number 1
-ob.asnum = 0
-ob.quiz(ob.qumber, ob.asnum)
 
-ob = Question(qumber = "2.", asnum = 1) # asnum starts from 0
-ob.qumber = "2."  # question number 2
-ob.asnum = 1
-ob.quiz(ob.qumber, ob.asnum)
+questions = 5 # set to your question number
 
-ob = Question(qumber = "3.", asnum = 2) # asnum starts from 0
-ob.qumber = "3."  # question number 3
-ob.asnum = 2
-ob.quiz(ob.qumber, ob.asnum)
+for i in range(0, questions):
+    ob = Question(qumber = f"{i+1}.", asnum = i) # asnum starts from 0
+    ob.qumber = f"{i+1}."  # question number i
+    ob.asnum = i
+    ob.quiz(ob.qumber, ob.asnum)
 
-ob = Question(qumber = "4.", asnum = 3) # asnum starts from 0
-ob.qumber = "4."  # question number 4
-ob.asnum = 3
-ob.quiz(ob.qumber, ob.asnum)
-
-ob = Question(qumber = "5.", asnum = 4) # asnum starts from 0
-ob.qumber = "5."  # question number 5
-ob.asnum = 4
-ob.quiz(ob.qumber, ob.asnum)
-
+print(f"You correctly answered {right_answers} questions!")
 '''
 add more questions if you need to.
 to add more questions simply copy the last line and change
